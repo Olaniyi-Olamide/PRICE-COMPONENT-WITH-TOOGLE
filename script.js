ed1fad6a3e4269f52c64c4861b1f9834a8b3885c
Olaniyi Olamide <olamideolaniyi616@gmail.com>
2025-10-15 19:32:00 +0100

    fix: toogle-annimation implemented properly

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 "use strict";
 const span = document.querySelectorAll("span");
 const toogleBtn = document.querySelector(".toogle-btn");
-const basicPriceAnnually = document.querySelector(".basic-price-annually");
-const basicPriceMonthly = document.querySelector(".basic-price-monthly");
-const professionalPriceAnnually = document.querySelector(
-  ".professional-price-annually"
-);
-const professionalPriceMonthly = document.querySelector(
-  ".professional-price-monthly"
-);
-const masterPriceAnnually = document.querySelector(".master-price-annually");
-const masterPriceMonthly = document.querySelector(".master-price-monthly");
+const basicPrice = document.querySelector(".basic-price");
+const professionalPrice = document.querySelector(".professional-price");
+const masterPrice = document.querySelector(".master-price");
 
-const tooglePriceAnnually = function (basic, professional, master) {
+const tooglePrice = function (basic, professional, master) {
   span.forEach((span) => {
     span.classList.add("text-annimation");
   });
@@ -20,38 +13,21 @@ const tooglePriceAnnually = function (basic, professional, master) {
     span.forEach((span) => {
       span.classList.remove("text-annimation");
     });
-    basic.classList.toggle("hidden");
-    professional.classList.toggle("hidden");
-    master.classList.toggle("hidden");
-  }, 1500);
-};
-
-const tooglePriceMonthly = function (basic, professional, master) {
-  span.forEach((span) => {
-    span.classList.add("text-annimation");
-  });
-  setTimeout(() => {
-    span.forEach((span) => {
-      span.classList.remove("text-annimation");
-    });
-    basic.classList.toggle("hidden");
-    professional.classList.toggle("hidden");
-    master.classList.toggle("hidden");
+    if (toogleBtn.classList.contains("toogle")) {
+      basic.innerHTML = "$19.99";
+      professional.innerHTML = "$24.99";
+      master.innerHTML = "$39.99";
+    }
+    if (!toogleBtn.classList.contains("toogle")) {
+      basic.innerHTML = "$199.99";
+      professional.innerHTML = "$249.99";
+      master.innerHTML = "$399.99";
+    }
   }, 1500);
 };
 
 toogleBtn.addEventListener("click", function () {
   toogleBtn.classList.toggle("toogle");
 
-  tooglePriceAnnually(
-    basicPriceAnnually,
-    professionalPriceAnnually,
-    masterPriceAnnually
-  );
-
-  tooglePriceMonthly(
-    basicPriceMonthly,
-    professionalPriceMonthly,
-    masterPriceMonthly
-  );
+  tooglePrice(basicPrice, professionalPrice, masterPrice);
 });
